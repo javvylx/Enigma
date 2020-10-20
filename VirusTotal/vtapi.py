1e937cfb8b41b2b305e69919f0c4dcdf4667a22b
damienllx <damienllx97@gmail.com>
2020-10-20 14:19:31 +0800

    Update vtapi.py

--- VirusTotal/vtapi.py
+++ VirusTotal/vtapi.py
@@ -41,34 +41,4 @@ with open(r'C://Users//damie//Desktop//Y2T1//ICT2202_Digital_Forensics//Assignme
         print("Index not found")
         exit(1)
     print("{}".format(final_results) + " positive signatures found.")
-
-
-
-
-
-
-#hash_list = [line.rstrip('\n') for line in hash_file]
-
-# for hash in hash_list:
-#     print(hash)
-#     response = vt.retrieve('2d75cc1bf8e57872781f9cd04a529256', raw=True, thing_type=hash)
-#     print(response)
-
-
-# vt = virustotal2.VirusTotal2("d265749382155a9250c2b1f2bc926eb6d7cfa0bd5c5ce00068225b516415cb67")
-# mdl_content = urlopen("http://www.malwaredomainlist.com/updatescsv.php", "r")
-# mdl_csv = csv.reader(mdl_content)
-#
-# for line in mdl_csv:
-#     ip=line[2].split("/")[0]
-#     try:
-#         ip_report = vt.retrieve(ip)   #get the VT IP report for this IP
-#     except:
-#         print ("API error: on ip " + ip)
-#
-#     total_pos = sum([u["positives"] for u in ip_report.detected_urls])
-#     total_scan = sum([u["total"] for u in ip_report.detected_urls])
-#     count = len(ip_report.detected_urls)
-#
-#     print (str(count)+" URLs hosted on "+ip+" are called malicious by (on average) " + \
-#           str(int(total_pos/count)) + " / " + str(int(total_scan/count)) + " scanners")
+    
