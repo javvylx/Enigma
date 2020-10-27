d6e45ef7da069410fce5a023056df9aa9b978c4c
Vangeance666 <55984328+vangeance666@users.noreply.github.com>
2020-10-28 01:06:39 +0800

    d

--- controller.py
+++ controller.py
@@ -6,37 +6,33 @@ class ModulesControler:
 
 
 	def __init__(self):
-		# Set up all variables from all modules
-
+		
 		# 
 		pass
-
-
 
 	def execute_ram_dump(self):
 		status = subprocess.run(self.RAM_DUMP_EXE_PATH, shell=True)
 
 
-	def get_image_profiles(self):
+	def triage_get_image_profiles(self):
+
 		# Parse from  Imageinfo.txt
-		# 
 		pass
 
-	def get_image_computer_info(self):
+	def triage_get_image_computer_info(self):
 		# Parse from computer_info.txt
-		# 
-		pass
-
-
-
-	def analyze_security_log(self, file_path):
-		# Call python module which feeds input to powershell
-		# Returns output in a json format for JS to process
 		
 		pass
 
 
-	def evaluate_exes_info(self):
+
+	def triage_analyze_security_log(self, file_path):
+		# Call python module which feeds input to powershell
+		# Returns output in a json format for JS to process		
+		pass
+
+
+	def triage_evaluate_exes_info(self):
 		pass
 
 
