eaabe563f0a59aad1b3a61f1c24506ca7874b545
kianwi <1902178@sit.singaporetech.edu.sg>
2020-10-28 01:59:45 +0800

    reuploaded welt

---
+++ WELT/cancer2202/operators/RP.ps1
@@ -0,0 +1,31 @@
+<#
+    REPEATED FAILED LOG IN EVENTS BRUTE FORCE
+#>
+
+
+$count = 0
+foreach($node in $nodes)
+{
+	$evid = $node.System.EventID.get_InnerXml()
+    $time = $node.System.TimeCreated.SystemTime
+    $evrid = $node.System.EventRecordID
+
+	if($evid -eq $triggerEvEventID -and $triggerEvOperator -eq "RP")
+		{
+			$count++
+			if($count -ge 3 -and $triggerEvEventID -eq $evid)
+				{
+					write-host "Multiple occurences of $evid found!" -foregroundcolor "darkyellow"
+		            $output = '{ RP triggered
+	"RuleTriggered: "'+$triggerRID+'",
+	"RuleName: "'+$rulename+'",
+	"EventID: "'+$evid+'",
+	"Timestamp: "'+$time+'",
+	"EventRecordID: "'+$evrid+'",
+	"Description: "'+$ruledesc+'",
+	}'
+		Add-content "$casefile\Analysis\Security_Analysis.json" $output
+				}
+		}
+
+}
